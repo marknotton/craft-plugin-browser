16b5a17c823b2624a6bc5ed9e82bab929d90e085
Mark Notton <mark@yellostudio.co.uk>
2017-07-04 12:40:18 +0100

    Added missing files

--- browser/vendor/composer/autoload_real.php
+++ browser/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit3f26c72f13ab7e3fad1c7d12fa9a070b
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit3f26c72f13ab7e3fad1c7d12fa9a070b', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
